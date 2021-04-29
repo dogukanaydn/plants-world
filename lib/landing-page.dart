67ac7405ce1f3929b3e4d73ae8ef506ad257ad4d
Doğukan Aydın <ddogukanaydin@gmail.com>
2021-04-29 21:06:42 +0300

    Deprecated FlatButton

--- lib/landing-page.dart
+++ lib/landing-page.dart
@@ -3,54 +3,57 @@ import 'package:flutter_svg/flutter_svg.dart';
 //import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plants_world/theme/constants.dart';
 
-class LandingPage extends StatelessWidget{
+class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: Container(
         color: AppConstants.lightPurple,
         height: size.height,
         width: double.infinity,
-        child: Column(
-          //alignment: Alignment.center,
-          //children: <Widget>[Image(image: AssetImage('assets/plant.jpg'),width: 200, height: 200,)],
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 150),
-              child: SvgPicture.asset("svg/Flowers-amico.svg",width: 200,height: 200),
+        child: Column(children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 150),
+            child: SvgPicture.asset("assets/svg/Flowers-amico.svg",
+                width: 200, height: 200),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 50),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                    backgroundColor: AppConstants.purple,
+                    minimumSize: Size(250, 50),
+                    primary: AppConstants.grey),
+                child: Text(
+                  "Login",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {},
+              ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 50),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: FlatButton(
-                  minWidth: 250,
-                  padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
-                  color: AppConstants.purple,
-                  onPressed: (){},
-                  child: Text("Login"),
-                  textColor: Colors.white,),),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    minimumSize: Size(250, 50),
+                    primary: AppConstants.grey),
+                child: Text(
+                  "Sign Up",
+                  style: TextStyle(color: AppConstants.purple),
+                ),
+                onPressed: () {},
+              ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 10),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: FlatButton(
-
-                  minWidth: 250,
-                  padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
-                  color: Colors.white,
-                  onPressed: (){},
-                  child: Text("Sign Up"),
-                  textColor: AppConstants.purple),),
-            )
-          //FlatButton(color: Colors.white ,onPressed: (){}, child: Text("Sign Up"), textColor: AppConstants.purple,),
+          )
         ]),
-
       ),
     );
   }
-
 }
