95b98bb13abc3814791b910f9650a7a7c9a2f9fa
Doğukan Aydın <ddogukanaydin@gmail.com>
2021-04-30 04:08:44 +0300

    Changed constant colors

--- lib/theme/constants.dart
+++ lib/theme/constants.dart
@@ -4,9 +4,9 @@ class AppConstants {
   static final purple = Color(0xFFBA68C8);
   static final lightPurple = Color(0xFFE8DAEF);
   static final green = Color(0xFF82E0AA);
-  static final lightRed = Color(0x80F30909);
+  static final lightRed = Color(0xFFF1948A);
   static final grey = Color(0xFFC4C4C4);
-  static final searchGrey = Color(0x80E0E0E0);
+  static final searchGrey = Color(0xFFE5E8E8);
 }
 
 class CustomTextHeadline {
