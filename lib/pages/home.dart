ee8bf0045d8e47b897fe7dc4e9b1b984d2d323a6
Doğukan Aydın <ddogukanaydin@gmail.com>
2021-04-30 03:15:46 +0300

    Added Home Page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:plants_world/theme/constants.dart';
 
 class Home extends StatefulWidget {
   Home({Key key}) : super(key: key);
@@ -20,12 +21,89 @@ class _HomeState extends State<Home> {
           style: TextStyle(color: Colors.black),
         ),
       ),
-      body: Center(
-        child: Text(
-          'Home Screen',
-          style: TextStyle(fontSize: 40),
-        ),
-      ),
+      body: ListViewHome(),
     );
+  }
+}
+
+class ListViewHome extends StatefulWidget {
+  @override
+  _ListViewHomeState createState() => _ListViewHomeState();
+}
+
+class _ListViewHomeState extends State<ListViewHome> {
+  bool isWatered = true;
+
+  final titles = [
+    "Kendi İsimlendirmen",
+    "Kendi İsimlendirmen",
+    "Kendi İsimlendirmen",
+    "Kendi İsimlendirmen",
+    "Kendi İsimlendirmen",
+    "Kendi İsimlendirmen",
+    "Kendi İsimlendirmen",
+    "Kendi İsimlendirmen",
+    "Kendi İsimlendirmen",
+    "Kendi İsimlendirmen",
+    "Kendi İsimlendirmen",
+    "Kendi İsimlendirmen",
+    "Kendi İsimlendirmen",
+    "Kendi İsimlendirmen",
+    "Kendi İsimlendirmen",
+    "Kendi İsimlendirmen",
+  ];
+
+  final subtitles = [
+    "Bitki Türünün İsmi",
+    "Bitki Türünün İsmi",
+    "Bitki Türünün İsmi",
+    "Bitki Türünün İsmi",
+    "Bitki Türünün İsmi",
+    "Bitki Türünün İsmi",
+    "Bitki Türünün İsmi",
+    "Bitki Türünün İsmi",
+    "Bitki Türünün İsmi",
+    "Bitki Türünün İsmi",
+    "Bitki Türünün İsmi",
+    "Bitki Türünün İsmi",
+    "Bitki Türünün İsmi",
+    "Bitki Türünün İsmi",
+    "Bitki Türünün İsmi",
+    "Bitki Türünün İsmi"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: titles.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Card(
+              color: AppConstants.lightPurple,
+              child: ListTile(
+                title: Text(
+                  titles[index],
+                  style: TextStyle(color: Colors.black),
+                ),
+                subtitle: Text(
+                  subtitles[index],
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w100,
+                      fontStyle: FontStyle.italic),
+                ),
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://images.unsplash.com/photo-1491147334573-44cbb4602074?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"),
+                ),
+                trailing: Icon(
+                  Icons.delete,
+                  color: AppConstants.lightRed,
+                ),
+              ),
+            ),
+          );
+        });
   }
 }
--- lib/theme/constants.dart
+++ lib/theme/constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppConstants {
   static final purple = Color(0xFFBA68C8);
-  static final lightPurple = Color(0x26BA68C8);
+  static final lightPurple = Color(0xFFE8DAEF);
   static final green = Color(0xFF82E0AA);
   static final lightRed = Color(0x80F30909);
   static final grey = Color(0xFFC4C4C4);
